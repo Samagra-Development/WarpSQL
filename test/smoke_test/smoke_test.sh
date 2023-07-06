d2970e76f015fc1e04839b973173068f97d59806
Piyush Raj <piyushraj92739@gmail.com>
2023-07-06 15:35:13 +0530

    added relative path to sub scripts

--- test/smoke_test/smoke_test.sh
+++ test/smoke_test/smoke_test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # 
 set -ex
-
+# get the base dir of the script 
+scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+echo $scriptDir/extentions_test
 # Find all test script files in the directory
-test_scripts=$(find ./extentions_test -type f -name 'test_*.sh')
+test_scripts=$(find $scriptDir/extentions_test -type f -name 'test_*.sh')
 
 # If no extensions provided, run all test scripts
 if [[ -z $1 ]]; then
@@ -13,7 +15,7 @@ if [[ -z $1 ]]; then
 else
 IFS=',' read -ra EXTENSION_LIST <<< "$1"
     for extension in "${EXTENSION_LIST[@]}"; do
-        test_script="./test_extentions/test_${extension}.sh"
+        test_script="$scriptDir/extentions_test/test_${extension}.sh"
         if [[ -f "$test_script" ]]; then
             bash "$test_script"
         else
