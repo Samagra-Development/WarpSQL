e0064a5e8bedad18f213bbff83bb488efa07d252
Piyush Raj <piyushraj92739@gmail.com>
2023-08-08 19:38:18 +0530

    terraform scripts for AWS AMI

---
+++ terraform/aws/main.tf
@@ -0,0 +1,104 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.42.0"
+    }
+  }
+  required_version = ">= 0.14.5"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr_vpc
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_subnet" "subnet_public" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.cidr_subnet
+}
+
+resource "aws_route_table" "rtb_public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta_subnet_public" {
+  subnet_id      = aws_subnet.subnet_public.id
+  route_table_id = aws_route_table.rtb_public.id
+}
+
+resource "aws_security_group" "sg_22_80" {
+  name_prefix = "warpsql"
+  vpc_id      = aws_vpc.vpc.id
+  # SSH access from the VPC
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # postgres access
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "tls_private_key" "warpsql-rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "aws_key_pair" "deployer" {
+  key_name_prefix = "warpsql"
+  public_key      = tls_private_key.warpsql-rsa.public_key_openssh
+}
+resource "local_sensitive_file" "pem_file" {
+  # write the private key to local for ssh access
+  filename        = pathexpand("./${aws_key_pair.deployer.key_name}.pem")
+  file_permission = "600"
+  content         = tls_private_key.warpsql-rsa.private_key_pem
+}
+resource "aws_instance" "web" {
+  ami                         = var.ami_id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.subnet_public.id
+  vpc_security_group_ids      = [aws_security_group.sg_22_80.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.deployer.key_name
+  user_data                   = <<EOF
+  #cloud-config
+
+  bootcmd:
+    - "POSTGRES_PASSWORD=${var.warpsql_password} PGDATA=/var/lib/postgesql/data /aws-alpine-entrypoint.sh postgres > /Warpsql.log  2>&1 &"
+  EOF
+  tags = {
+    Name = "WarpSQL"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
