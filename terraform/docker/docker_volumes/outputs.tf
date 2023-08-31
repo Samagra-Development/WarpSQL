f1b9f446d6a17dd40366556ed55e55d839804126
Piyush Raj <piyushraj92739@gmail.com>
2023-08-31 18:31:20 +0530

    remove docker

--- terraform/docker/docker_volumes/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "volume-postgres" {
-  value = docker_volume.warpsql-postgres
-}
-output "volume-barman" {
-  value = docker_volume.warpsql-barman
-}
--- terraform/docker/main.tf
+++
@@ -1,7 +0,0 @@
-module "warpsql-volumes" {
-  source = "./docker_volumes"
-}
-
-module "warpsql-containers" {
-  source = "./docker_containers"
-}
