e54b337c4357b74c6d910011b30e7cdeccac1fb1
Piyush Raj <piyushraj92739@gmail.com>
2023-11-07 14:11:23 +0530

    fix test

--- test/smoke_test/extentions_test/test_pg_cron.sh
+++ test/smoke_test/extentions_test/test_pg_cron.sh
@@ -2,5 +2,7 @@
 set -ex 
 
 echo "Test pg_cron Extension"
-psql -c "CREATE EXTENSION pg_cron;"
-psql -c "SELECT cron.schedule('30 3 * * 6', $$DELETE FROM events WHERE event_time < now() - interval '1 week'$$);"
+psql <<"EOF"
+CREATE EXTENSION pg_cron;
+SELECT cron.schedule('30 3 * * 6',$$DELETE FROM events WHERE event_time < now() - interval '1 week'$$);
+EOF
